5dcfd8f31bfdb503d3d6e29a494db49c79edf914
chceswieta <chceswieta@gmail.com>
2022-06-11 12:02:32 +0200

    Add working Schnorr signature checking

--- server/src/server/crypto.py
+++ server/src/server/crypto.py
@@ -1,15 +1,17 @@
+from typing import Tuple
+
+from Cryptodome.Hash import SHA3_256
 from Cryptodome.PublicKey import ECC
-from common import CURVE_NAME
+from common import CURVE_NAME, CURVE_ORD, CURVE_G
 
 
 class Crypto:
     @staticmethod
-    def verify_schnorr_signature(client_id: int, signature: str, exponent: int, public_key: str) -> bool:
+    def verify_schnorr_signature(client_id: int, signature: Tuple[int, int], exponent: int, public_key: Tuple[int, int]) -> bool:
         """Check if the client's Schnorr signature is valid, i.e. if the ZKP for the first phase passed."""
-        # publicznie wysylamy A = g^sekret
-        # chcemy wyslac X = g^rand i s = rand - sekret * hash(i)
-        # wtedy serwer sprawdza czy g^rand = g^s * A^hash(i)
-        signature = ECC.import_key(encoded=signature, curve_name=CURVE_NAME).pointQ
-        signature_check = ECC.construct(curve=CURVE_NAME, d=exponent).pointQ
-        public_key = ECC.import_key(encoded=public_key, curve_name=CURVE_NAME).pointQ
-        return signature == signature_check + public_key * hash(client_id)
+        h = SHA3_256.new()
+        h.update(bytes(client_id))
+        e = int(h.hexdigest(), base=16) % CURVE_ORD
+        signature = ECC.construct(curve=CURVE_NAME, point_x=signature[0], point_y=signature[1]).pointQ
+        public_key = ECC.construct(curve=CURVE_NAME, point_x=public_key[0], point_y=public_key[1]).pointQ
+        return signature == CURVE_G * exponent + public_key * e
